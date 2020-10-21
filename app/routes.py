61fa1cc05bebd6b795ee050a78dfcdf7af9f1219
manksharma <2mayanksharma2@gmail.com>
2020-10-21 13:56:46 -0700

    added templates and gitignore

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
+@app.route('/home')
 def index():
-    return "Hello world!"
+    return render_template('home.html')
 
 @app.route('/favicon.ico')
 def favicon():
